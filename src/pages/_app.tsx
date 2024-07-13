3d5ad07b4edd7286df6258faf32e7e29b4a4b242
Joel Kasisi <joelkasisi10@gmail.com>
2024-07-13 04:19:32 +0300

    added svg and name

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ function MyApp({ Component, pageProps}: AppProps) {
   const { currentWeek } = useWeekNavigation();
   return (
     <PlansProvider currentWeek={currentWeek}>
+       <title>Perspektiva</title>
+       <link rel="icon" href="/flexed-biceps.svg" />
       <Component {...pageProps} />
     </PlansProvider>
   );
