167d877f165fdeaac84b84ae453401a02bd165d2
Joel Kasisi <joelkasisi10@gmail.com>
2024-07-16 18:19:35 +0300

    added error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+import styles from '../styles/components/Error/404.module.scss';
+
+const Custom404: React.FC = () => {
+  const router = useRouter();
+
+  const handleGoHome = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>404 - Page Not Found</h1>
+        <p className={styles.message}>Sorry, the page you are looking for does not exist.</p>
+        <button className={styles.button} onClick={handleGoHome}>
+          Go to Home
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Custom404;
