86fd0af31179edc3e3c82dd1f730b56bba25e57c
Joel Kasisi <joelkasisi10@gmail.com>
2024-07-17 12:00:48 +0300

    added firebase

--- Firebase-config.ts
+++ Firebase-config.ts
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBpimSyGU83yTEZC8pT_gxNUZ32yo8GM5A",
-  authDomain: "percentbetter-80e3e.firebaseapp.com",
-  projectId: "percentbetter-80e3e",
-  storageBucket: "percentbetter-80e3e.appspot.com",
-  messagingSenderId: "362365269883",
-  appId: "1:362365269883:web:5b52e7b3096858c12b6fde",
-  measurementId: "G-F65JBV56JX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
